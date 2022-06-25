99ae0af92bf0f8deaaab2c87e76427676c716436
Alexander R <ru5nedug@mail.ru>
2022-06-25 16:53:21 +0300

    sort

--- src/Sort.tsx
+++ src/Sort.tsx
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const Sort = () => {
+    const sort = ['популярности', 'цене', 'алфавиту',];
+
+    const [index, setIndex] = useState(0);
+    const [isVisible, setIsVisible] = useState(false);
+
+    const clickSortHandler = (index: number) => () => {
+        setIndex(index);
+        setIsVisible(false);
+    };
+
+    const clickItemSortHandler = () => {
+        setIsVisible(!isVisible);
+    };
+
     return (
         <div className="sort">
             <div className="sort__label">
@@ -11,21 +25,24 @@ export const Sort = () => {
                     fill="none"
                     xmlns="http://www.w3.org/2000/svg"
                 >
-                    <path
+                    <path transform = {isVisible ? 'scale(1, -1) translate(0, -6)' : undefined}
                         d="M10 5C10 5.16927 9.93815 5.31576 9.81445 5.43945C9.69075 5.56315 9.54427 5.625 9.375 5.625H0.625C0.455729 5.625 0.309245 5.56315 0.185547 5.43945C0.061849 5.31576 0 5.16927 0 5C0 4.83073 0.061849 4.68424 0.185547 4.56055L4.56055 0.185547C4.68424 0.061849 4.83073 0 5 0C5.16927 0 5.31576 0.061849 5.43945 0.185547L9.81445 4.56055C9.93815 4.68424 10 4.83073 10 5Z"
                         fill="#2C2C2C"
                     />
                 </svg>
                 <b>Сортировка по:</b>
-                <span>популярности</span>
+                <span onClick={clickItemSortHandler}>{sort[index]}</span>
             </div>
-            <div className="sort__popup">
+
+            {isVisible && <div className={'sort__popup'}>
                 <ul>
-                    <li className="active">популярности</li>
-                    <li>цене</li>
-                    <li>алфавиту</li>
+                    {
+                        sort.map((li, i) =>
+                            <li key={i} className={index === i ? 'active' : ''} onClick={clickSortHandler(i)}>{li}</li>)
+
+                    }
                 </ul>
-            </div>
+            </div>}
         </div>
     );
 };
