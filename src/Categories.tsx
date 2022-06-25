edefce4e1d9ea137e6167523b681b18801d58a78
Alexander R <ru5nedug@mail.ru>
2022-06-25 12:49:02 +0300

    Categories

--- src/Categories.tsx
+++ src/Categories.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const Categories = () => {
+    const [index, setIndex] = useState(0);
+
+    const clickHandler = (index: number) => {
+        setIndex(index);
+    };
+
     return (
         <div className="categories">
             <ul>
-                <li className="active">Все</li>
-                <li>Мясные</li>
-                <li>Вегетарианская</li>
-                <li>Гриль</li>
-                <li>Острые</li>
-                <li>Закрытые</li>
+                <li className={index === 0 ? 'active' : ''} onClick={() => clickHandler(0)}>Все</li>
+                <li className={index === 1 ? 'active' : ''} onClick={() => clickHandler(1)}>Мясные</li>
+                <li className={index === 2 ? 'active' : ''} onClick={() => clickHandler(2)}>Вегетарианская</li>
+                <li className={index === 3 ? 'active' : ''} onClick={() => clickHandler(3)}>Гриль</li>
+                <li className={index === 4 ? 'active' : ''} onClick={() => clickHandler(4)}>Острые</li>
+                <li className={index === 5 ? 'active' : ''} onClick={() => clickHandler(5)}>Закрытые</li>
             </ul>
         </div>
     );
