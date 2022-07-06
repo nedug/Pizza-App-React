118990e958c53fc68aae554cec91ced9614490a7
Alexander R <ru5nedug@mail.ru>
2022-07-06 18:08:27 +0300

    gh-pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './state/store';
 
@@ -11,8 +11,10 @@ const root = ReactDOM.createRoot(
 );
 root.render(
     <Provider store={store}>
-        <BrowserRouter>
+        {/*<BrowserRouter>*/}
+        <HashRouter>
             <App />
-        </BrowserRouter>
+        </HashRouter>
+        {/*</BrowserRouter>*/}
     </Provider>
 );
